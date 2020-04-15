d3c3e6dc277a1548f9be61bcf4ab60175932cce8
Aibol Izbassar <aibolizbassarov@gmail.com>
2020-04-15 20:36:31 +0600

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections;
+
+namespace XORarray
+{
+  
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DynamicArray ar = new DynamicArray(10);
+            XORdecorate xr = new XORdecorate(ar); // Передаю в XOR декоратор
+
+            xr.AddValue(20);
+            xr.AddValue(25);
+            xr.AddValue(45);
+            xr.AddValue(90);
+
+            
+            for (int i = 0; i < xr.GetLength; i++)
+            {
+                Console.WriteLine("После XOR: " + xr.GetValue(i));
+            }
+
+            for (int i = 0; i < xr.GetLength; i++)
+            {
+                Console.WriteLine("После расшифровки: " + xr.GetValueWithReXOR(i));
+            }
+            
+        }
+    }
+}
